c4032dbb9be1913582df95ba43a60d5ccf47cfd3
Stephen Mwangi <mail@stephenmwangi.com>
2026-03-29 12:23:03 +0300

    chore: update clstr version

--- Formula/clstr.rb
+++ Formula/clstr.rb
@@ -1,7 +1,7 @@
 class Clstr < Formula
     desc "Learn distributed systems by building them from scratch"
     homepage "https://clstr.io/"
-    url "https://github.com/clstr-io/clstr.git", tag: "v0.3.0"
+    url "https://github.com/clstr-io/clstr.git", tag: "v0.4.0"
 
     depends_on "go" => :build
 
