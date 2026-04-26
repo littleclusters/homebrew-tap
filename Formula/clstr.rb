55f32a4f3e4109169441f731c50ee8b886518eb2
Stephen Mwangi <st3v3n.mw@gmail.com>
2026-04-26 08:24:37 +0000

    chore: bump clstr to v0.5.2

--- Formula/clstr.rb
+++ Formula/clstr.rb
@@ -1,7 +1,7 @@
 class Clstr < Formula
     desc "Learn distributed systems by building them from scratch"
     homepage "https://clstr.io/"
-    url "https://github.com/clstr-io/clstr.git", tag: "v0.5.1"
+    url "https://github.com/clstr-io/clstr.git", tag: "v0.5.2"
 
     depends_on "go" => :build
 
